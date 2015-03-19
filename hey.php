27674df87d439ef9f11ea41618da9bd0bcf82bc5
Dan R <danromuald@drmbanga.com>
2015-03-19 19:37:49 +0200

    Update hey.php
    fix semi colon and stop drinking...

--- hey.php
+++ hey.php
@@ -3,6 +3,6 @@
   <title> I am the drunk php test page</title>
   </head>
   <body>
-  <?php echo '<h1><p> Hey there, The test worked! Now let me get back to my beers!</p></h1> ?>
+  <?php echo '<h1><p> Hey there, The test worked! Now let me get back to my beers!</p></h1>'; ?>
   </body>
 </html>
