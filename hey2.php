20cdf532ef6ac3027ca9a25ea13991b732769e6a
Dan R <danromuald@drmbanga.com>
2015-04-06 13:57:48 +0000

    adding a secong hey file for git push testing

---
+++ hey2.php
@@ -0,0 +1,8 @@
+<html>
+  <head>
+  <title> I am the drunk php test page</title>
+  </head>
+  <body>
+  <?php echo '<h1><p> Hey there, The test worked! Now let me get back to my beers!</p></h1>'; ?>
+  </body>
+</html>
