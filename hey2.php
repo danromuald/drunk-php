fdb1f897db7283476b858bbf6967b28e53877d0c
Dan R <danromuald@drmbanga.com>
2015-04-06 14:00:14 +0000

    removing the test hey2 file

--- hey2.php
+++
@@ -1,8 +0,0 @@
-<html>
-  <head>
-  <title> I am the drunk php test page</title>
-  </head>
-  <body>
-  <?php echo '<h1><p> Hey there, The test worked! Now let me get back to my beers!</p></h1>'; ?>
-  </body>
-</html>
